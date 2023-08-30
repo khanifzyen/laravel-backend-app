f49b2d41903b823ee701e3a71cc49cfcf5684136
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2023-08-30 23:06:34 +0700

    add error validation login page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //return view('welcome');
-    return view('pages.blank-page',['type_menu'=>'']);
+    return view('auth.login');
+});
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('home',function(){
+        return view('pages.blank-page',['type_menu'=>'']);
+    })->name('home');
 });
 
 // Route::get('/login', function () {
