2dbfae45eaa6672a93da7eca1398189d3f193605
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2023-08-30 07:26:30 +0700

    add user seeder

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,7 @@
                         <label for="password"
                             class="control-label">Password</label>
                         <div class="float-right">
-                            <a href="auth-forgot-password.html"
+                            <a href="{{ route('password.request')}}"
                                 class="text-small">
                                 Forgot Password?
                             </a>
@@ -56,18 +56,6 @@
                 </div>
 
                 <div class="form-group">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox"
-                            name="remember"
-                            class="custom-control-input"
-                            tabindex="3"
-                            id="remember-me">
-                        <label class="custom-control-label"
-                            for="remember-me">Remember Me</label>
-                    </div>
-                </div>
-
-                <div class="form-group">
                     <button type="submit"
                         class="btn btn-primary btn-lg btn-block"
                         tabindex="4">
@@ -75,26 +63,10 @@
                     </button>
                 </div>
             </form>
-            <div class="mt-4 mb-3 text-center">
-                <div class="text-job text-muted">Login With Social</div>
-            </div>
-            <div class="row sm-gutters">
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-facebook">
-                        <span class="fab fa-facebook"></span> Facebook
-                    </a>
-                </div>
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-twitter">
-                        <span class="fab fa-twitter"></span> Twitter
-                    </a>
-                </div>
-            </div>
-
         </div>
     </div>
     <div class="text-muted mt-5 text-center">
-        Don't have an account? <a href="auth-register.html">Create One</a>
+        Don't have an account? <a href="{{ route('register')}}">Create One</a>
     </div>
 @endsection
 
